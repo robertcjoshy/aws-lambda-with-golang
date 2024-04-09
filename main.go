914c88cfb30ce8688289ea4dca71a62d579bad8c
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-09 23:53:34 +0530

    code

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var Database *gorm.DB
+
+const (
+	host     = "database-robert.chsu6q8eqfk8.ap-south-1.rds.amazonaws.com"
+	port     = 5432
+	user     = "postgres"
+	password = "daisyjoshy"
 )
 
 type MyEvent struct {
@@ -16,7 +27,16 @@ type MyResponse struct {
 }
 
 func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s id %d years old", event.Name, event.Age)}, nil
+	var err error
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s", host, port, user, password)
+
+	Database, err = gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	} else {
+		return MyResponse{Message: fmt.Sprintf("%s id %d years old", event.Name, event.Age)}, nil
+	}
 }
 func main() {
 	lambda.Start(HandleLambdaEvent)
